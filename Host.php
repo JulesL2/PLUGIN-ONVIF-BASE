4a29a07df6f156e03a392abc723b27bf1ef0b6a9
Jules Lecoustre <38831521+JulesL2@users.noreply.github.com>
2018-06-03 18:03:54 +0200

    Update Host.php

--- Host.php
+++ Host.php
@@ -3,75 +3,24 @@
 require_once __DIR__ . '/../../../script/core/ressources/HostONVIF.php';
 
 require_once "HostONVIF.php";
-function json_validate($_test)
-    {
-    // Decode pour test erreur
-    $result = json_decode($_test);
 
-    // Test erreur
-	
-	if(json_last_error() != JSON_ERROR_NONE) 
-	{
-        throw new Exception(JSONErrorMap::get(json_last_error()));
-		 // Exit sur exeption
-		echo "Erreur \n";
-		echo $_error;
-	}
-	// Fin sans erreur
-    $_test2 = json_decode($_test);
-	if ($_test2 =='null' || $_test2 =='')
-	{
-	throw new Exception('Fichier Vide');
-	}
+$camera_un = new HostONVIF();
 
-	echo "Aucune erreur \n";  
-    
-    }
-
-
-$hostid = new HostONVIF();
-
-
-// LE TABLEAU DATA EST A REMPLACER AVEC LES VALEURS TROUVE DANS JEEDOM
 $_data = 
     [
+// VALEURS PAR DEFAUT
+// A CHANGER AVEC LES GET DE JEEDOM
+  'Username' => 'admin',
 
-  'Username' => 'USER',
+  'Password' => 'admin',
 
-  'Password' => 'PASSWORD',
+  'IPadress' => '192.168.1.X',
 
-  'IPadress' => 'IP',
-
-  'Port' => 'PORT',
+  'Port' => '1234',
     ];
 
 
-$hostid->hydrate($_data);
+$camera_un->hydrate($_data);
 
+$camera_un->gethost();
 
-echo "Le nom d'utilisateur est: ",$hostid->getUsername(),"\n";
-echo "Le mot de passe est: ",$hostid->getPassword(),"\n";
-echo "L'adresse ip de la camera est: ",$hostid->getIPadress(),"\n";
-echo "Le port est: ",$hostid->getPort(),"\n";
-
-$hostid -> Gethost();
-
-echo "Passage a l'execution du node \n";
-
-$host = shell_exec('node gethost.js');
-
-
-// TEST DE LA VALIDITE DE LA SORTIE DU SCRIPT
-$sortiehost = json_validate($host);
-print_r($sortiehost);
-
-// SI LE FICHIER EST VALIDE ALORS
-$hostjson = json_decode($host, true);
-
-// NOM SUR LE RESEAU
-$name = $hostjson['host']['name'];
-
-// DISPLAY NAME
-echo "HOST \n";
-echo "Nom ",$name,"\n";
-echo "\n";
