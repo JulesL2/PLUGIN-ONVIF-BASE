3aeaa6bdccb4b34db46a9e45b6fd6ca3e26ec42c
Jules Lecoustre <38831521+JulesL2@users.noreply.github.com>
2018-06-03 22:15:57 +0200

    Update Camera.php

--- PHP/Camera.php
+++ PHP/Camera.php
@@ -19,8 +19,31 @@ $_data =
   'Port' => '1234',
     ];
 
-
 $camera_un->hydrate($_data);
-
+/*
 $camera_un->gethost();
 
+$camera_un->getdeviceinfo();
+
+$camera_un->getimage();
+
+$camera_un->getnodes();
+
+$camera_un->getstream();
+
+$camera_un->getsnap();
+
+$camera_un->getprofile();
+
+$camera_un->getpresets();
+
+$camera_un->relativemove(1,1,1,1,1,1);
+
+$camera_un -> setpreset(test);
+
+$camera_un -> removepreset(10);
+
+$camera_un -> gotopreset(1);
+
+$camera_un -> setimagesettings(40,40,40,40);
+*/
